f7907fa8c8d531f101532e0fa81486a4067591e1
blee1999 <99771997+blee1999@users.noreply.github.com>
2022-05-04 11:27:27 -0500

    Update fuelquote.php

--- finalproject/fuelquote.php
+++ finalproject/fuelquote.php
@@ -2,6 +2,8 @@
 //include auth_session.php file on all user panel pages
 include("auth_session.php");
 ob_start();
+require('db.php');
+
 ?>
 
 <html lang="en">
@@ -18,15 +20,13 @@ ob_start();
 
    <form class="form-group centered" action="" method="REQUEST">
         <input class="form-control" type="number" name="gallons" id="gallons" placeholder="Gallons of Fuel" required></input><br><br>
-        <input class="form-control" type="text" name="address" id="address" placeholder="Address" required readonly></input><br><br>
+        <input class="form-control" type="text" name="address" id="address" placeholder="<?php echo $_SESSION['state']; ?>" required readonly></input><br><br>
         <input class="form-control" type="date" name="date" id="date" placeholder="Date" ></input><br><br>
         <input class="form-control" type="number" name="sprice" id="sprice" placeholder="Suggested Price per Gallon" required readonly></input><br><br>
         <input class="form-control" type="number" name="totalPrice" id="totalPrice" placeholder="Total_Price" required readonly></input><br><br>
         <input class="form-control" type="submit" name="Submit" id="Submit" value="Submit"></input><br>
                         <br><br>
                 <a href="profile.php">Profile</a>
-        
-
    </form><br><br>
    </div>
   </div>
@@ -34,16 +34,20 @@ ob_start();
 </body>
 </html>
 <?php
-    require('db.php');
     if(isset($_REQUEST['Submit']))
     {
         $username = $_SESSION['username'];
         $state = $_SESSION['state'];
         $date = $_REQUEST['date'];
+        $currentdate = date('Y-m-d');;
         $gallons = $_REQUEST['gallons'];
+        $gallons = max($gallons,0);
         $margin = 0;    
         $current_price = 1.5;
         
+        if($date < $currentdate){
+            echo  ' <script type="text/javascript"> alert("Date is in the past") </script> ';
+        }
         //if gallons is greater than 1000 or less than 1000
         if($gallons >= 1000){
             $margin = $margin + .02;
@@ -71,20 +75,23 @@ ob_start();
         $totalPrice = $sprice * $gallons;
 
 
-
-        $query = "INSERT INTO `fuel` (username, state, date, gallons, totalPrice) 
-        VALUES('$username', '$state', '$date', '$gallons', '$totalPrice')";
-
-        $query_run = mysqli_query($con,$query);
-
-        if($query_run)
-        {
-            echo  ' <script type="text/javascript"> alert("Data Saved") </script> ';
-
-        }
-        else
-        {
-            echo  ' <script type="text/javascript"> alert("Data not Saved") </script> ';
+        if($date > $currentdate) {
+            $query = "INSERT INTO `fuel` (username, state, date, gallons, totalPrice) 
+            VALUES('$username', '$state', '$date', '$gallons', '$totalPrice')";
+    
+            $query_run = mysqli_query($con,$query);
+            
+            if($query_run)
+            {
+            echo "<div class='form'>
+                  <h3>Price = $totalPrice.</h3><br/>
+                  <p class='link'>Click here to <a href='profile.php'>Return to profile page</a> again.</p>
+                  </div>";
+            }
+            else
+            {
+                echo  ' <script type="text/javascript"> alert("Data not Saved") </script> ';
+            }
         }
     }
 ?>
